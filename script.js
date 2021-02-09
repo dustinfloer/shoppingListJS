c63cc9acbe795ff43d10cfeea2f9e77b4af60db7
Dustin Floer <dustinfloer@dustins-air.lan>
2021-02-09 10:56:09 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+var input = document.getElementById("userinput"); 
+var button = document.getElementById("enter");
+var ul = document.querySelector("ul");
+
+function inputLength () {
+	return input.value.length;
+}
+
+function createListElement () {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+}
+
+button.addEventListener ("click", function() {
+	if (inputLength() > 0) {
+		createListElement();
+	} 
+})
+
+input.addEventListener ("keypress", function(event) {
+	if (inputLength() > 0 && event.which === 13) {
+		createListElement();
+	} 
+})
